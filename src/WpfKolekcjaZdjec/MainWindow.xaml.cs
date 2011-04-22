5cf46baed5d9d43a6f59d14fc6db931e845cd586
lizka <elizkra@gmail.com>
2011-04-22 18:01:07 +0200

    ikony

---
+++ src/WpfKolekcjaZdjec/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKolekcjaZdjec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void image1_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void image2_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void image4_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void radMaskedTextBox1_ValueChanged(object sender, Telerik.Windows.RadRoutedEventArgs e)
+        {
+
+        }
+
+        private void image1_ImageFailed_1(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void radButton1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void image8_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+    }
+}
