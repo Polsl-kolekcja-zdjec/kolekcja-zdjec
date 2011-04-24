2651f19f95c5f2de29264aaebef4eb6632f30663
lizka <elizkra@gmail.com>
2011-04-24 23:32:29 +0200

    gui poprawki

---
+++ src/WpfKolekcjaZdjec/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfKolekcjaZdjec
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
--- src/WpfKolekcjaZdjec/MainWindow.xaml.cs
+++ src/WpfKolekcjaZdjec/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Telerik.Windows.Controls;
 
 namespace WpfKolekcjaZdjec
 {
@@ -22,6 +23,7 @@ namespace WpfKolekcjaZdjec
         public MainWindow()
         {
             InitializeComponent();
+            miniaturki.Visibility = System.Windows.Visibility.Hidden;
         }
 
         private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
@@ -41,7 +43,7 @@ namespace WpfKolekcjaZdjec
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void image4_ImageFailed(object sender, ExceptionRoutedEventArgs e)
@@ -64,9 +66,111 @@ namespace WpfKolekcjaZdjec
 
         }
 
-        private void image8_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        private void burn_ImageFailed(object sender, ExceptionRoutedEventArgs e)
         {
 
         }
+
+        private void RadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void import_Click(object sender, RoutedEventArgs e)
+        {
+
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void export_Click(object sender, RoutedEventArgs e)
+        {
+
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+
+
+        private void personal_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void burn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void image4_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void image5_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void report_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            RadWindow.Alert("do uzupełnienia");
+        }
+
+        private void islajdshow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            miniaturki.Visibility = System.Windows.Visibility.Hidden;
+            slajdshow.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void thumbails_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            miniaturki.Visibility = System.Windows.Visibility.Visible;
+            slajdshow.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+
+        void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (e.NewValue > 0.03 && e.NewValue < 0.97)
+            {
+                this.Panel.TopItemPathFraction = e.NewValue;
+                this.scaleCenterPoint.PathFraction = e.NewValue;
+                this.opacityCenterPoint.PathFraction = e.NewValue;
+                this.opacityLeftPoint.PathFraction = e.NewValue - 0.03;
+                this.opacityRightPoint.PathFraction = e.NewValue + 0.03;
+            }
+            else if (e.NewValue <= 0.5)
+            {
+                this.Slider_ValueChanged(this, new RoutedPropertyChangedEventArgs<double>(0, 0.04));
+            }
+            else if (e.NewValue >= 0.97)
+            {
+                this.Slider_ValueChanged(this, new RoutedPropertyChangedEventArgs<double>(0, 0.96));
+            }
+        }
+
+        private void Windows_Init(object sender, EventArgs e)
+        {
+            this.Visibility = System.Windows.Visibility.Hidden;
+            Login l = new Login();
+            bool wartoscDialogResult = (bool)l.ShowDialog();
+            if (wartoscDialogResult)
+                this.Visibility = System.Windows.Visibility.Visible;
+            else
+                this.Close();
+        }
+
+        private void details_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            miniaturki.Visibility = System.Windows.Visibility.Hidden;
+            slajdshow.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Zamknij_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        
+
+     
     }
 }
