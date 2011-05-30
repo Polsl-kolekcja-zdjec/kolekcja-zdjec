5effde001130b3ac7ee0b48a24604abff54351ab
Wojtek 'afronski' Gawronski <afronski@gmail.com>
2011-05-30 20:12:00 +0200

    Connection strings helper and new methods for DA.

---
+++ src/WpfKolekcjaZdjec/Business/ConnectionStringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace WpfKolekcjaZdjec.Business
+{
+    /// <summary>
+    /// Static helper for connection strings.
+    /// </summary>
+    public static class ConnectionStringHelper
+    {
+        /// <summary>
+        /// Get actual connection string.
+        /// </summary>
+        /// <returns>Connection string value.</returns>
+        public static string GetActualConnectionString()
+        {
+            return ConfigurationManager.AppSettings["PhotoCollectionDatabaseEntities"].ToString();
+        }
+    }
+}
